a8dd08558e5bd6bb22a454f16242bd69bd1a2bc5
ShuvoKumerSingh <87059001+ShuvoKumerSingh@users.noreply.github.com>
2022-09-10 22:12:05 +0600

    Delete factorial.py

--- Recursion/factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    assert n>=1 and int(n) == n, "please give the integer number and 0 i not given"
-    if n in [0,1]:
-        return 1
-    else:
-        return (n*factorial(n-1))
-result = factorial(4)
-print(result)
