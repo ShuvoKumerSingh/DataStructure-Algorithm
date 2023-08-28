28ea9dcab0f7d09773847515af1c71d9a1140987
ShuvoKumerSingh <87059001+ShuvoKumerSingh@users.noreply.github.com>
2023-08-28 16:13:03 +0600

    duplicate.py
    Remove Duplicates from a Singly Linked List

---
+++ LinkedList/RemoveDuplicate/duplicate.py
@@ -0,0 +1,50 @@
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    
+    def __str__(self):
+        temp_node = self.head
+        result = ''
+        while temp_node is not None:
+            result += str(temp_node.value)
+            if temp_node.next is not None:
+                result += ' -> '
+            temp_node = temp_node.next
+        return result
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+    
+    def remove_duplicates(self):
+        # TODO
+        if self.head is None:
+            return
+
+        nodeValues = set()
+        currentNode = self.head
+        nodeValues.add(currentNode.value)
+
+        while currentNode.next :
+            if currentNode.next.value in nodeValues:
+                currentNode.next = currentNode.next.next
+                self.length-=1
+
+            else:
+                nodeValues.add(currentNode.next.value)
+                currentNode = currentNode.next
+        self.tail = currentNode
